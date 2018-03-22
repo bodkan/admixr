80049ecde7b84edd85424f907bd2dffaba694a09
Martin Petr <git@bodkan.net>
2018-03-22 22:12:53 +0100

    Check for ADMIXTOOLS on load (closes #19)

---
+++ R/zzz.R
@@ -0,0 +1,11 @@
+.onLoad <- function(libname, pkgname) {
+  # check for presence of an ADMIXTOOLS command on user's PATH and display
+  # a warning if it's not present
+  path_check <- system("command -v qpDstat", ignore.stdout = TRUE)
+  if (path_check != 0) {
+    packageStartupMessage(
+      "ADMIXTOOLS binaries could not be found in your $PATH.\n",
+      "Consider modifying the $PATH variable in your .Renviron file."
+    )
+  }
+}
