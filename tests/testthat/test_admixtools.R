385b9e4c396210376f47713d23a33ce9ef4ca475
Martin Petr <git@bodkan.net>
2018-07-10 16:58:48 +0200

    Update tests

--- tests/testthat/test_admixtools.R
+++ tests/testthat/test_admixtools.R
@@ -4,5 +4,5 @@ admixtools_present <- function() {
   system("which qpDstat", ignore.stdout = TRUE) == 0
 }
 
-test_that("ADMIXTOOLS is present", { expect_equal(admixtools_present(), TRUE) })
-test_that("ADMIXTOOLS data is present", { expect_equal(dir.exists(data_path()), TRUE) })
+test_that("ADMIXTOOLS is present", { expect_true(admixtools_present()) })
+test_that("ADMIXTOOLS data is present", { expect_true(dir.exists(data_path())) })
