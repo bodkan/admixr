879c6744d1db9c33f9b4cfd1ff4adda9dc017b91
Martin Petr <git@bodkan.net>
2018-07-31 00:56:44 +0200

    Fix SNP coverage parsing from the output

--- R/output_parsers.R
+++ R/output_parsers.R
@@ -166,13 +166,15 @@ read_qpAdm <- function(file) {
   leftpops <- stringr::str_locate(log_lines, "(left|right) pops:") %>%
     .[, 1] %>%  { !is.na(.) } %>% which
 
-  snp_count <- stringr::str_subset(log_lines, "numsnps used:") %>%
-    stringr::str_replace("numsnps used: ", "") %>%
+  target_pop <- log_lines[leftpops[1] + 1]
+  source_pops <- log_lines[(leftpops[1] + 2) : (leftpops[2] - 2)]
+
+  snp_count <- stringr::str_subset(log_lines, glue::glue("coverage: +{target_pop}")) %>%
+    stringr::str_replace(glue::glue("coverage: +{target_pop} +"), "") %>%
     as.numeric
 
-  # FIX types -- everything is character here
-  rbind(c(log_lines[leftpops[1] + 1], snp_count, admix_props)) %>%
+  rbind(c(target_pop, snp_count, admix_props)) %>%
     tibble::as_tibble() %>%
-    setNames(c("target", "snp_count", log_lines[(leftpops[1] + 2) : (leftpops[2] - 2)])) %>%
+    setNames(c("target", "snp_count", source_pops)) %>%
     dplyr::mutate_at(dplyr::vars(-target), as.numeric)
 }
