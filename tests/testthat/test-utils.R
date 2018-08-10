6258692f010fae29c753e49e2802b28489f38b40
Martin Petr <git@bodkan.net>
2018-08-10 23:33:41 +0200

    Add tests for check_presence

---
+++ tests/testthat/test-utils.R
@@ -0,0 +1,19 @@
+context("Utility functions")
+
+prefix <- file.path(admixtools_path(), "data", "allmap")
+
+
+# Checking for presence of a sample in an ind file ------------------------
+
+test_that("Fails to find a missing sample", {
+  expect_error(check_presence("blah", ind = paste0(prefix, ".ind")))
+  expect_error(check_presence("blah", prefix))
+  expect_error(check_presence(c("blah1", "blah2"), prefix))
+  expect_error(check_presence(c("blah1", "blah2", "French"), prefix))
+})
+
+test_that("Succeeds in finding a sample in an ind file", {
+  expect_silent(check_presence("French", ind = paste0(prefix, ".ind")))
+  expect_silent(check_presence("French", prefix))
+  expect_silent(check_presence(c("French", "Neander"), prefix))
+})
