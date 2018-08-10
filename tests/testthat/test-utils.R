1a65bb36cc93db4e6e1d5f7b9382349c2ced84bf
Martin Petr <git@bodkan.net>
2018-08-10 23:37:25 +0200

    Update tests

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,11 +1,9 @@
 context("Utility functions")
 
-prefix <- file.path(admixtools_path(), "data", "allmap")
-
-
 # Checking for presence of a sample in an ind file ------------------------
 
 test_that("Fails to find a missing sample", {
+  prefix <- file.path(admixtools_path(), "data", "allmap")
   expect_error(check_presence("blah", ind = paste0(prefix, ".ind")))
   expect_error(check_presence("blah", prefix))
   expect_error(check_presence(c("blah1", "blah2"), prefix))
@@ -13,6 +11,7 @@ test_that("Fails to find a missing sample", {
 })
 
 test_that("Succeeds in finding a sample in an ind file", {
+  prefix <- file.path(admixtools_path(), "data", "allmap")
   expect_silent(check_presence("French", ind = paste0(prefix, ".ind")))
   expect_silent(check_presence("French", prefix))
   expect_silent(check_presence(c("French", "Neander"), prefix))
