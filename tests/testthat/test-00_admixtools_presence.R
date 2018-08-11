2d468db904c2267a43913bdd28b3bf6564ce552b
Martin Petr <git@bodkan.net>
2018-08-11 11:09:16 +0200

    Try printing $PATH to verify ADMIXTOOLS is present

--- tests/testthat/test-00_admixtools_presence.R
+++ tests/testthat/test-00_admixtools_presence.R
@@ -7,5 +7,5 @@ admixtools_present <- function() {
 path <- admixtools_path()
 data_dir <- file.path(path, "data")
 
-test_that("ADMIXTOOLS is present", { expect_true(admixtools_present()) })
+test_that(paste0("ADMIXTOOLS is present", Sys.getenv("PATH")), { expect_true(admixtools_present()) })
 test_that("ADMIXTOOLS data is present", { expect_true(dir.exists(data_dir)) })
