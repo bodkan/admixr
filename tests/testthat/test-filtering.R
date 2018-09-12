bac79b5400db4ededf9d92dad126b0f6ac129f05
Martin Petr <git@bodkan.net>
2018-09-13 01:21:00 +0200

    Add test

--- tests/testthat/test-filtering.R
+++ tests/testthat/test-filtering.R
@@ -26,47 +26,47 @@ system(sprintf("tail -n+2 %s > %s", orig_data$snp, data$snp))
 system(sprintf("tail -n+2 %s > %s", orig_data$geno, data$geno))
 
 test_that("filter_sites correctly fails at no overlap", {
-    # create a BED file that has the same positions as the original EIGENSTRAT
-    bed <- tempfile()
-    snp_to_bed(data$snp, bed)
+  # create a BED file that has the same positions as the original EIGENSTRAT
+  bed <- tempfile()
+  snp_to_bed(data$snp, bed)
 
-    # verify that no overlaps leads to error
-    expect_error(filter_bed(data, bed, remove = TRUE))
+  # verify that no overlaps leads to error
+  expect_error(filter_bed(data, bed, remove = TRUE))
 })
 
 test_that("filter_sites correctly handles complete overlap", {
-    # create a BED file that has the same positions as the original EIGENSTRAT
-    bed <- tempfile()
-    snp_to_bed(data$snp, bed)
+  # create a BED file that has the same positions as the original EIGENSTRAT
+  bed <- tempfile()
+  snp_to_bed(data$snp, bed)
 
-    # generate a "subset" based on that BED file
-    output <- tempfile()
-    new_data <- filter_bed(data = data, bed = bed)
+  # generate a "subset" based on that BED file
+  output <- tempfile()
+  new_data <- filter_bed(data = data, bed = bed)
 
-    # verify that both EIGENSTRAT datasets are the same
-    expect_true(nrow(read_snp_file(new_data$exclude)) == 0)
+  # verify that both EIGENSTRAT datasets are the same
+  expect_true(nrow(read_snp_file(new_data$exclude)) == 0)
 })
 
 test_that("Overlap returns a correct number of sites", {
-    snp <- read_snp_file(data$snp)
+  snp <- read_snp_file(data$snp)
 
-    # resample a BED file a number of times, verifying that we get the correct
-    # number of sites after the overlap operation
-    successes <- sapply(seq_len(nrow(snp) - 1), function(n) {
-        # create a BED file that has a subset of sites from the original snp file
-        bed <- tempfile()
-        snp %>%
-          dplyr::mutate(start = pos - 1, end = pos) %>%
-          dplyr::select(chrom, start, end) %>%
-          dplyr::sample_n(nrow(snp) - n) %>%
-          dplyr::arrange() %>%
-          readr::write_tsv(bed, col_names = FALSE)
-        # generate a "subset" based on thad BED file
-        new_data <- filter_bed(data, bed)
+  # resample a BED file a number of times, verifying that we get the correct
+  # number of sites after the overlap operation
+  successes <- sapply(seq_len(nrow(snp) - 1), function(n) {
+    # create a BED file that has a subset of sites from the original snp file
+    bed <- tempfile()
+    snp %>%
+      dplyr::mutate(start = pos - 1, end = pos) %>%
+      dplyr::select(chrom, start, end) %>%
+      dplyr::sample_n(nrow(snp) - n) %>%
+      dplyr::arrange() %>%
+      readr::write_tsv(bed, col_names = FALSE)
+    # generate a "subset" based on thad BED file
+    new_data <- filter_bed(data, bed)
 
-        nrow(read_snp_file(new_data$exclude)) == n
-    })
-    expect_true(all(successes))
+    nrow(read_snp_file(new_data$exclude)) == n
+  })
+  expect_true(all(successes))
 })
 
 test_that("Resetting returns an EIGENSTRAT object to an original state", {
@@ -79,4 +79,15 @@ test_that("Resetting returns an EIGENSTRAT object to an original state", {
                          function(i) data[[i]] == new_data[[i]])))
   expect_true(is.null(new_data$groups))
   expect_true(is.null(new_data$exclude))
+})
+
+test_that("Filtering works when data is piped into a calculation", {
+  data_dir <- file.path(admixtools_path(), "data")
+  examples_dir <- file.path(admixtools_path(), "examples")
+
+  data <- eigenstrat(file.path(data_dir, "allmap"))
+  pops <- read_pops(file.path(examples_dir, "list_qpDstat1"), c("W", "X", "Y", "Z"))
+  expect_silent(
+    data %>% remove_transitions %>% d(W = pops$W, X = pops$X, Y = pops$Y, Z = pops$Z)
+  )
 })
