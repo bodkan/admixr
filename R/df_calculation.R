cd0e7b3d3084001a65fa0315f6eb55e8521372bc
Martin Petr <git@bodkan.net>
2018-11-30 16:59:05 +0100

    Update

--- R/df_calculation.R
+++ R/df_calculation.R
@@ -1,31 +1,28 @@
-# Calculate f4 statistic from a data.frame.
-f4_df <- function(sites, w, x, y, z) {
-  a <- sample_freq(sites, w)
-  b <- sample_freq(sites, x)
-  c <- sample_freq(sites, y)
-  d <- sample_freq(sites, z)
+f4_df <- function(sites, W, X, Y, Z) {
+  a <- sample_freq(sites, W)
+  b <- sample_freq(sites, X)
+  c <- sample_freq(sites, Y)
+  d <- sample_freq(sites, Z)
   
   mean((a - b) * (c - d), na.rm = TRUE)
 }
 
 
 
-# Calculate f4-ratio statistic from a data.frame.
-f4ratio_df <- function(sites, x, a, b, c, o) {
+f4ratio_df <- function(sites, X, A, B, C, O) {
   tibble::tibble(
-    name = x,
-    nea = purrr::map_dbl(x, ~ f4_df(sites, a, o, .x, c) / f4_df(sites, a, o, b, c))
+    name = X,
+    nea = purrr::map_dbl(X, ~ f4_df(sites, A, O, .x, C) / f4_df(sites, A, O, B, C))
   )
 }
 
 
 
-# Calculate D statistic from a data.frame.
-d_df <- function(sites, w, x, y, z) {
-  a <- sample_freq(sites, w)
-  b <- sample_freq(sites, x)
-  c <- sample_freq(sites, y)
-  d <- sample_freq(sites, z)
+d_df <- function(sites, W, X, Y, Z) {
+  a <- sample_freq(sites, W)
+  b <- sample_freq(sites, X)
+  c <- sample_freq(sites, Y)
+  d <- sample_freq(sites, Z)
   
   nom <- sum((a - b) * (c - d), na.rm = TRUE)
   denom <- sum((a + b - 2 * a * b) * (c + d - 2 * c * d), na.rm = TRUE)
@@ -35,33 +32,6 @@ d_df <- function(sites, w, x, y, z) {
 
 
 
-#' Calculate f3 statistics given four sets of samples.
-#'
-#' @param sites data.table with simulated SNPs
-#' @param a Character vector of sample names.
-#' @param b Character vector of sample names.
-#' @param c Character vector of sample names.
-#'
-#' @return f3 statistic
-#'
-#' @export
-f3_df <- function(sites, A, B, C) {
-  a <- sample_freq(sites, A)
-  b <- sample_freq(sites, B)
-  c <- sample_freq(sites, C)
-  
-  mean((a - c) * (b - c), na.rm = TRUE)
-}
-
-
-
-#' Calculate allele frequencies of a given set of samples at a given set
-#' of sites.
-#'
-#' @param sites data.table with genotypes.
-#' @param samples Character vector of sample names.
-#'
-#' @import data.table
 sample_freq <- function(sites, samples) {
-  1 - (base::rowMeans(sites[, samples]) / 2)
+  base::rowMeans(sites[, samples]) / 2
 }
