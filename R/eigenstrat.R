1e10ab54d25e4c5b1686a612d4dc234106f5dbf6
Martin Petr <git@bodkan.net>
2019-10-16 10:53:46 +0200

    Revert back `wc -l` SNP counting

--- R/eigenstrat.R
+++ R/eigenstrat.R
@@ -294,8 +294,10 @@ process_filter <- function(data, exclude, outfile) {
 # and excluded SNPs.
 add_excluded_snps <- function(data, snpfile) {
     data$exclude <- path.expand(snpfile)
-    data$n_excluded <- count_lines(data$exclude)
-    data$n_included <- count_lines(data$snp) - data$n_excluded
+    ## data$n_excluded <- count_lines(data$exclude)
+    ## data$n_included <- count_lines(data$snp) - data$n_excluded
+    data$n_excluded <- nrow(read_snp(data, exclude = TRUE))
+    data$n_included <- nrow(read_snp(data)) - data$n_excluded
     data
 }
 
