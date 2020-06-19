b08d67a52c645e577ff550c67d7a388a7521b072
Martin Petr <git@bodkan.net>
2020-06-19 21:26:55 +0200

    Add qpAdm_rotation test

---
+++ tests/testthat/test-qpAdm.R
@@ -0,0 +1,64 @@
+# These tests run the same calculations as those performed by ADMIXTOOLS'
+# built in tests (see test-01_admixtools_cmds.R), but they do so using
+# admixr's wrapper functions. The results of both tests (i.e. contents of
+# output log files) are then compared to each other.
+
+context("qpAdm rotation functionality")
+
+if (admixtools_present()) {
+    snps <- eigenstrat(download_data())
+    ## modeling with full output
+    models1 <- qpAdm_rotation(
+        data = snps,
+        target = "French",
+        candidates = c("Dinka", "Mbuti", "Yoruba", "Vindija", "Altai"),
+        minimize = FALSE,
+        nsources = 2,
+        ncores = 1,
+        fulloutput = TRUE
+    )
+    ## modelling with minimal output
+    models2 <- qpAdm_rotation(
+        data = snps,
+        target = "French",
+        candidates = c("Dinka", "Mbuti", "Yoruba", "Vindija", "Altai"),
+        minimize = FALSE,
+        nsources = 2,
+        ncores = 1,
+        fulloutput = FALSE
+    )
+}
+
+# f3 ----------------------------------------------------------------------
+
+# qpAdm -------------------------------------------------------------------
+
+test_that("qpAdm_rotation() output object is of the right class", {
+    skip_on_cran()
+    skip_on_os("windows")
+    expect_true(inherits(models1, "admixr_result") & attr(models1, "command") == "qpAdm_rotation")
+})
+
+
+test_that("qpAdm_rotation() `fulloutput = TRUE` object is of the right format", {
+    skip_on_cran()
+    skip_on_os("windows")
+    expect_equal(sort(names(models1)), sort(c("proportions", "ranks", "subsets")))
+})
+
+
+test_that("qpAdm_rotation() `fulloutput = FALSE` object is of the right format", {
+    skip_on_cran()
+    skip_on_os("windows")
+    expect_true(is.data.frame(models2$proportions))
+})
+
+
+test_that("qpAdm_rotation() output object carries all required annotation", {
+    skip_on_cran()
+    skip_on_os("windows")
+    expect_true(!is.null(attr(models1, "log_output")))
+})
+
+
+
