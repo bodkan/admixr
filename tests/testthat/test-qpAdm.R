609ec69cd911ac9957137f43a00624cb4820f3a7
Martin Petr <git@bodkan.net>
2020-06-21 22:26:38 +0200

    Expand unit tests

--- tests/testthat/test-qpAdm.R
+++ tests/testthat/test-qpAdm.R
@@ -7,7 +7,12 @@ context("qpAdm rotation functionality")
 
 if (admixtools_present()) {
     snps <- eigenstrat(download_data())
-    ## modeling with full output
+}
+
+test_that("qpAdm_rotation() `fulloutput = TRUE` object is of the right format", {
+    skip_on_cran()
+    skip_on_os("windows")
+
     models1 <- qpAdm_rotation(
         data = snps,
         target = "French",
@@ -17,7 +22,14 @@ if (admixtools_present()) {
         ncores = 1,
         fulloutput = TRUE
     )
-    ## modelling with minimal output
+    expect_equal(sort(names(models1)), sort(c("proportions", "ranks", "subsets")))
+})
+
+
+test_that("qpAdm_rotation() `fulloutput = FALSE` object is of the right format", {
+    skip_on_cran()
+    skip_on_os("windows")
+
     models2 <- qpAdm_rotation(
         data = snps,
         target = "French",
@@ -27,18 +39,6 @@ if (admixtools_present()) {
         ncores = 1,
         fulloutput = FALSE
     )
-}
-
-test_that("qpAdm_rotation() `fulloutput = TRUE` object is of the right format", {
-    skip_on_cran()
-    skip_on_os("windows")
-    expect_equal(sort(names(models1)), sort(c("proportions", "ranks", "subsets")))
-})
-
-
-test_that("qpAdm_rotation() `fulloutput = FALSE` object is of the right format", {
-    skip_on_cran()
-    skip_on_os("windows")
     expect_true(is.data.frame(models2))
 })
 
@@ -46,6 +46,16 @@ test_that("qpAdm_rotation() `fulloutput = FALSE` object is of the right format",
 test_that("qpAdm_rotation() output object carries all required annotation", {
     skip_on_cran()
     skip_on_os("windows")
+
+    models1 <- qpAdm_rotation(
+        data = snps,
+        target = "French",
+        candidates = c("Dinka", "Mbuti", "Yoruba", "Vindija", "Altai"),
+        minimize = FALSE,
+        nsources = 2,
+        ncores = 1,
+        fulloutput = TRUE
+    )
     expect_true(!is.null(attr(models1, "log_output")))
 })
 
@@ -53,6 +63,15 @@ test_that("qpAdm_rotation() output object carries all required annotation", {
 test_that("qpAdm_filter() filters p-value and proportions", {
     skip_on_cran()
     skip_on_os("windows")
+    models1 <- qpAdm_rotation(
+        data = snps,
+        target = "French",
+        candidates = c("Dinka", "Mbuti", "Yoruba", "Vindija", "Altai"),
+        minimize = FALSE,
+        nsources = 2,
+        ncores = 1,
+        fulloutput = TRUE
+    )
     filtered <- qpAdm_filter(models1, p = 0.01)
     expect_true(all(c(filtered$proportions$pvalue > 0.01,
                       filtered$proportions$prop1 < 1,
@@ -62,16 +81,22 @@ test_that("qpAdm_filter() filters p-value and proportions", {
 })
 
 
-test_that("It is possible to switch off outgroup number minimization", {
+test_that("It is possible to switch on outgroup number minimization", {
     models <- qpAdm_rotation(
         data = snps,
         target = "French",
         candidates = c("Dinka", "Mbuti", "Yoruba", "Vindija", "Altai"),
-        minimize = FALSE,
+        minimize = TRUE,
         nsources = 2,
         ncores = 1,
         fulloutput = TRUE
     )
-    expect_true(unique(models$proportions$noutgroups) == 3)
+    expect_true(sort(unique(models$proportions$noutgroups)) == c(2, 3))
 })
 
+
+test_that("qpAdm_fiter works only on objects of qpAdm_rotation type", {
+    x <- data.frame()
+    class(x) <- "random"
+    expect_error(qpAdm_filter(x))
+})
