8671af7e0c2cd590996f923f557fda4006c8ade4
Martin Petr <git@bodkan.net>
2022-08-01 21:48:09 +0200

    Implement qpAdm_rotation `nsources > 2` tests

--- tests/testthat/test-wrappers.R
+++ tests/testthat/test-wrappers.R
@@ -83,11 +83,10 @@ test_that("qpAdm wrapper produces correct results", {
   expect_equal(props1, props2)
 })
 
-
 test_that("qpAdm with a single source produces NULL subsets dataframe", {
   skip_on_cran()
   skip_on_os("windows")
-  
+
   data <- eigenstrat(file.path(data_dir, "qpdata"))
   left <- scan(file.path(examples_dir, "left1"), what = "character", quiet = TRUE)[1:2]
   right <- scan(file.path(examples_dir, "right1"), what = "character", quiet = TRUE) %>%
@@ -96,6 +95,52 @@ test_that("qpAdm with a single source produces NULL subsets dataframe", {
   expect_true(is.null(result$subsets))
 })
 
+test_that("qpAdm gives the correct number of source/stderr columns (n = 2)", {
+  skip_on_cran()
+  skip_on_os("windows")
+
+  # two sources
+  nsources <- 2
+  sources2 <- qpAdm_rotation(
+    data = snps,
+    target = "French",
+    candidates = c("Dinka", "Mbuti", "Yoruba", "Vindija", "Altai"),
+    minimize = FALSE,
+    nsources = nsources,
+    ncores = 1,
+    fulloutput = TRUE
+  )
+  expect_true(all(unique(sources2$ranks$rank) == (nsources - 1) : nsources))
+  expect_true(all(grep("source", colnames(sources2$proportions), value = TRUE) == paste0("source", 1:nsources)))
+
+  # three sources
+  nsources <- 3
+  sources3 <- qpAdm_rotation(
+    data = snps,
+    target = "French",
+    candidates = c("Dinka", "Mbuti", "Yoruba", "Vindija", "Altai", "Denisova", "Sardinian"),
+    minimize = FALSE,
+    nsources = nsources,
+    ncores = 1,
+    fulloutput = TRUE
+  )
+  expect_true(all(unique(sources3$ranks$rank) == (nsources - 1) : nsources))
+  expect_true(all(grep("source", colnames(sources3$proportions), value = TRUE) == paste0("source", 1:nsources)))
+
+  # four sources
+  nsources <- 4
+  sources4 <- qpAdm_rotation(
+    data = snps,
+    target = "French",
+    candidates = c("Dinka", "Mbuti", "Yoruba", "Vindija", "Altai", "Sardinian", "Khomani_San", "Chimp", "Han"),
+    minimize = FALSE,
+    nsources = nsources,
+    ncores = 1,
+    fulloutput = TRUE
+  )
+  expect_true(all(unique(sources4$ranks$rank) == (nsources - 1) : nsources))
+  expect_true(all(grep("source", colnames(sources4$proportions), value = TRUE) == paste0("source", 1:nsources)))
+})
 
 # qpWave ------------------------------------------------------------------
 
