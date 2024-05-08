b72798b2e94b95c1ac713f11c16eedb7eb7895ce
Martin Petr <git@bodkan.net>
2024-05-08 19:18:19 +0200

    Remove deprecated keep_transitions

--- R/eigenstrat.R
+++ R/eigenstrat.R
@@ -204,23 +204,6 @@ filter_bed <- function(data, bed, remove = FALSE, outfile = tempfile(fileext = "
 #' @param outfile Path to an output snp file with coordinates of excluded sites.
 #'
 #' @return Updated S3 EIGENSTRAT data object with an additional 'exclude' slot
-#'     specifying the path to the set of SNPs to be removed.
-#'
-#' @export
-keep_transitions <- function(data, outfile = tempfile(fileext = ".snp")) {
-  .Deprecated("transversions_only")
-  transversions_only(data, outfile)
-}
-
-#' Remove transversions (C->T and G->A substitutions)
-#'
-#' Remove substitutions that are more likely to be a result of ancient DNA
-#' damage (C->T and G->A substitutions).
-#'
-#' @param data EIGENSTRAT data object.
-#' @param outfile Path to an output snp file with coordinates of excluded sites.
-#'
-#' @return Updated S3 EIGENSTRAT data object with an additional 'exclude' slot
 #'     specifying the path to the set of SNPs to be removed from a downstream
 #'     analysis.
 #'
